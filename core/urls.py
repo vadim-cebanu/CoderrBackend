ec66a065c4b89b3e4c542b4b44e59ada3e21232b
vadim-cebanu <vadykdeejay@gmail.com>
2026-07-20 17:09:08 +0200

    Add core/urls.py (Implement orders app with CRUD endpoints and permissions)

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('auth_app.urls')),
     path('api/', include('offers_app.urls')),
+    path('api/', include('orders_app.urls')),
+
 ]
