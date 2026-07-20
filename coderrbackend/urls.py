994ca79d20d24ada7b6d1f11449201cc85980c4d
vadim-cebanu <vadykdeejay@gmail.com>
2026-07-20 15:50:08 +0200

    Add coderrbackend/urls.py (Add profile management endpoints)

--- coderrbackend/urls.py
+++ coderrbackend/urls.py
@@ -4,9 +4,14 @@ from . import views
 
 
 router = DefaultRouter()
+router.register(r'profile', views.ProfileViewSet, basename='profile')
+
 
 urlpatterns = [
     path('registration/', views.registration_view, name='registration'),
     path('login/', views.login_view, name='login'),
+    path('profiles/business/', views.business_profiles_view, name='business-profiles'),
+    path('profiles/customer/', views.customer_profiles_view, name='customer-profiles'),
+
     path('', include(router.urls)),
 ]
